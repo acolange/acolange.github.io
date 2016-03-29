f2b0a46cf25c5b5e06125d00df4b8a56d6c4b959
A.J. Colangelo <acolange@gmail.com>
2016-03-28 21:40:38 -0700

    adding backside

--- js/resume.js
+++ js/resume.js
@@ -1,17 +1,11 @@
 $(document).ready( function() {
     $(".billboard").click( function() {
-      $(this).addClass("spin");
+      $(".icons").fadeOut('slow', function() {
+        $('.billboard').css('background-color', '#535353');
+        $('.cardInfo').show();
+      });
     });
-
-    // $('.spinner').animate({  textIndent: 0 }, {
-  //     step: function(go) {
-  //       $(this).css('-moz-transform','rotateY('+go+'deg)');
-  //       $(this).css('-webkit-transform','rotateY('+go+'deg)');
-  //       $(this).css('-o-transform','rotateY('+go+'deg)');
-  //       $(this).css('transform','rotateY('+go+'deg)');
-  //     },
-  //     duration: 3000,
-  //     complete: function(){ alert('done') }
-  // });
-
+    $('.cardInfo').click( function() {
+      $('.billboard').animate({height: '1098px'}, 1000);
+    });
 });
